271a8fe618a488d1fae8cab820826f81476e48f7
Ancelli <xnazar507@gmail.com>
2025-02-26 12:01:26 +0200

    Scriptable Objects

---
+++ Assets/ScriptableObjects/Scripts/Plant.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField] private PlantData info;
+    SetPlantInfo spi;
+
+    void Start()
+    {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.plantName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (info.Threat.ToString() == "High")
+        {
+            PlayerController1.dead = true;
+        }
+    }
+}
