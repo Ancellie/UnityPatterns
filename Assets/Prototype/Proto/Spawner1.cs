2decf73e495a4eb2a0b8119db940f98f5a73a391
Ancelli <xnazar507@gmail.com>
2025-05-28 20:39:11 +0300

    Some update

--- Assets/Prototype/Proto/Spawner1.cs
+++ Assets/Prototype/Proto/Spawner1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class Spawner1 : MonoBehaviour
 {
     public GameObject cubeprefab;
     public GameObject sphereprefab;
