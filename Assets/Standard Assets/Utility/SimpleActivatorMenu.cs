ca468114865975b92161cb9f2c1a1acb439b6b09
Ancelli <xnazar507@gmail.com>
2025-06-02 11:43:13 +0300

    Deleted GUITexture

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,37 +1,32 @@
-using System;
 using UnityEngine;
+using UnityEngine.UI; // Додати для нового UI
 
 namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
-        // An incredibly simple menu which, when given references
-        // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        // Використовуємо UI.Text замість застарілого GUIText
+        public Text camSwitchButton;  // <-- Потрібно призначити в інспекторі
         public GameObject[] objects;
-
 
         private int m_CurrentActiveObject;
 
-
         private void OnEnable()
         {
-            // active object starts from first in array
             m_CurrentActiveObject = 0;
             camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
 
-
         public void NextCamera()
         {
-            int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
+            int nextActiveObject = (m_CurrentActiveObject + 1) % objects.Length;
 
             for (int i = 0; i < objects.Length; i++)
             {
-                objects[i].SetActive(i == nextactiveobject);
+                objects[i].SetActive(i == nextActiveObject);
             }
 
-            m_CurrentActiveObject = nextactiveobject;
+            m_CurrentActiveObject = nextActiveObject;
             camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
     }
