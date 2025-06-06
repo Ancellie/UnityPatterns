083663df04e51c56cd69e7a806af4d2f166cad8c
Ancelli <xnazar507@gmail.com>
2025-06-06 17:16:08 +0300

    Finished StatePattern

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,32 +1,37 @@
+using System;
 using UnityEngine;
-using UnityEngine.UI; // Додати для нового UI
 
 namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
-        // Використовуємо UI.Text замість застарілого GUIText
-        public Text camSwitchButton;  // <-- Потрібно призначити в інспекторі
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+        public GUIText camSwitchButton;
         public GameObject[] objects;
+
 
         private int m_CurrentActiveObject;
 
+
         private void OnEnable()
         {
+            // active object starts from first in array
             m_CurrentActiveObject = 0;
             camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
 
+
         public void NextCamera()
         {
-            int nextActiveObject = (m_CurrentActiveObject + 1) % objects.Length;
+            int nextactiveobject = m_CurrentActiveObject + 1 >= objects.Length ? 0 : m_CurrentActiveObject + 1;
 
             for (int i = 0; i < objects.Length; i++)
             {
-                objects[i].SetActive(i == nextActiveObject);
+                objects[i].SetActive(i == nextactiveobject);
             }
 
-            m_CurrentActiveObject = nextActiveObject;
+            m_CurrentActiveObject = nextactiveobject;
             camSwitchButton.text = objects[m_CurrentActiveObject].name;
         }
     }
