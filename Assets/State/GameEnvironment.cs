33a6ad11974fe44d394eb92545ab0300d9ebc0d3
Ancelli <xnazar507@gmail.com>
2025-06-08 16:34:27 +0300

    Finished ObjectPool

--- Assets/State/GameEnvironment.cs
+++ Assets/State/GameEnvironment.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -25,3 +26,4 @@ public sealed class GameEnvironment
         }
     }
 }
+*/
